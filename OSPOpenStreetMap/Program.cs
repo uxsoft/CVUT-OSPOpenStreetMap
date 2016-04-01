c481bbd1474d80cdb75e80231d1ca84e5bea7f8a
Jan Dryk <me@uxsoft.cz>
2016-04-01 13:52:16 +0200

    improved command line input

--- OSPOpenStreetMap/Program.cs
+++ OSPOpenStreetMap/Program.cs
@@ -7,31 +7,61 @@ using System.Text;
 using MoreLinq;
 using System.Threading.Tasks;
 using System.Device.Location;
+using System.IO;
+using System.Text.RegularExpressions;
+using OsmSharp.Streams;
 
 namespace OSPOpenStreetMapTester
 {
     class Program
     {
-        const string PBFPATH = @"C:\Users\me\Downloads\czech-republic-latest.osm.pbf";
-
         static void Main(string[] args)
         {
             Stopwatch watch = new Stopwatch();
             watch.Start();
 
-            Console.WriteLine("Please specify the tag you want your ways filtered by");
-            Console.WriteLine("Key: ");
-            string tagKey = Console.ReadLine();
-            Console.WriteLine("Value: ");
-            string tagValue = Console.ReadLine();
-            Console.WriteLine($"Processing all ways tagged {tagKey}={tagValue}");
-
-            using (var file = System.IO.File.OpenRead(PBFPATH))
+            //Input file
+            string filePath = "", tagKey = "", tagValue = "";
+            if (args.Any() && File.Exists(args.First()))
+                filePath = args.First();
+            else
             {
-                var source = new OsmSharp.Streams.PBFOsmStreamSource(file);
+                do
+                {
+                    Console.WriteLine("Please specify the location of your OSM  data PBF file:");
+                    filePath = Console.ReadLine();
+                } while (!File.Exists(filePath));
+            }
 
-                var totalLength = ProcessWays(new WayEnumerable(source)
-                    .Where(w => w.Tags.ContainsKey(tagKey) && w.Tags[tagKey] == tagValue));
+            //Input filter
+            if (args.Length > 0)
+            {
+                var match = Regex.Match(args.Last(), "(?<key>[a-z]+)=(?<value>[a-z]+)");
+                if (match.Success)
+                {
+                    tagKey = match.Groups["key"].Value;
+                    tagValue = match.Groups["value"].Value;
+                }
+            }
+            if (string.IsNullOrWhiteSpace(tagKey) || string.IsNullOrWhiteSpace(tagValue))
+            {
+                Console.WriteLine("Please specify the tag you want your ways filtered by");
+                Console.WriteLine("Key: ");
+                tagKey = Console.ReadLine();
+                Console.WriteLine("Value: ");
+                tagValue = Console.ReadLine();
+                Console.WriteLine($"Processing all ways tagged {tagKey}={tagValue}");
+            }
+
+            //Process
+            using (var waysStream = File.OpenRead(filePath))
+            using (var nodesStream = File.OpenRead(filePath))
+            {
+                var ways = new WayEnumerable(new PBFOsmStreamSource(waysStream))
+                    .Where(w => w.Tags.ContainsKey(tagKey) && w.Tags[tagKey] == tagValue)
+                    .ToArray();
+
+                var totalLength = ProcessWays(ways, new PBFOsmStreamSource(nodesStream));
 
                 Console.WriteLine($"Total length: {Math.Floor(totalLength / 1000)}km");
             }
@@ -40,27 +70,27 @@ namespace OSPOpenStreetMapTester
             Console.ReadLine();
         }
 
-        private static double ProcessWays(IEnumerable<Way> ways)
+        private static double ProcessWays(IEnumerable<Way> ways, OsmStreamSource source)
         {
-            var waysFixed = ways.ToArray();
+            var nodesNeeded = ways
+                .SelectMany(w => w.Nodes)
+                .Distinct();
+            var nodes = FindNodes(nodesNeeded, source);
 
-            var nodesNeeded = waysFixed.SelectMany(w => w.Nodes).Distinct();
-            var nodes = FindNodes(nodesNeeded);
-
-            return waysFixed.Select(w =>
+            return ways.Select(w =>
                 w.Nodes
                     .Select(id => nodes[id])
-                    .Pairwise((from, to) => DistanceEstimateInMeter(from.Item1, from.Item2, to.Item1, to.Item2))
+                    .Pairwise((from, to) => Distance(from, to))
                     .Sum()
             ).Sum();
         }
 
-        public static double DistanceEstimateInMeter(double latitude1, double longitude1, double latitude2, double longitude2)
+        public static double Distance(Tuple<double, double> from, Tuple<double, double> to)
         {
-            return new GeoCoordinate(latitude1, longitude1).GetDistanceTo(new GeoCoordinate(latitude2, longitude2));
+            return new GeoCoordinate(from.Item1, from.Item2).GetDistanceTo(new GeoCoordinate(to.Item1, to.Item2));
         }
 
-        public static Dictionary<long, Tuple<double, double>> FindNodes(IEnumerable<long> nodeIds)
+        public static Dictionary<long, Tuple<double, double>> FindNodes(IEnumerable<long> nodeIds, OsmStreamSource source)
         {
             Queue<long> idQueue = new Queue<long>();
             foreach (long id in nodeIds.OrderBy(n => n))
@@ -68,20 +98,16 @@ namespace OSPOpenStreetMapTester
 
             var nodes = new Dictionary<long, Tuple<double, double>>();
 
-            using (var file = System.IO.File.OpenRead(PBFPATH))
+            while (idQueue.Count > 0 && source.MoveNextNode())
             {
-                var source = new OsmSharp.Streams.PBFOsmStreamSource(file);
-                while (idQueue.Count > 0 && source.MoveNextNode())
+                if (source.Current().Id == idQueue.Peek())
                 {
-                    if (source.Current().Id == idQueue.Peek())
-                    {
-                        nodes.Add(idQueue.Dequeue(),
-                            new Tuple<double, double>((source.Current() as Node).Latitude.Value,
-                                (source.Current() as Node).Longitude.Value));
-                    }
+                    nodes.Add(idQueue.Dequeue(),
+                        new Tuple<double, double>((source.Current() as Node).Latitude.Value,
+                            (source.Current() as Node).Longitude.Value));
                 }
-                return nodes;
             }
+            return nodes;
         }
     }
 }
